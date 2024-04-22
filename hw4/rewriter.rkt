9f04551757bd6b03243dfe6d859db52b32ec1101
Tanner Helton <tanner@tannerhelton.com>
2024-04-22 13:35:58 -0500

    still stuck on currify

--- hw4/rewriter.rkt
+++ hw4/rewriter.rkt
@@ -15,23 +15,30 @@
      (cond->if-helper cs)]
     [_ (transform e)]))
 
-
 (define (currify e)
   (define (curry-lambda xs body)
     (if (null? xs)
-        (transform body)
+        (begin
+          (displayln "Currying terminal lambda body")
+          (transform body))
         (let ([first (car xs)]
               [rest (cdr xs)])
+          (displayln (format "Currying lambda with arg: ~a" first))
           (Lam (list first) (curry-lambda rest body)))))
-  
+
   (define (curry-application f args)
-    (foldl (lambda (acc arg) (App acc (transform arg)))
-           (transform f)
-           args))
-  
+    (define transformed-f (transform f))
+    (define transformed-args (map transform args))
+    (displayln "Currying application")
+    (foldr (lambda (arg acc) (App acc arg)) transformed-f transformed-args))
+
   (match e
-    [(Lam xs body) (curry-lambda xs body)]
-    [(App f args) (curry-application f args)]
+    [(Lam xs body)
+     (displayln (format "Currying lambda with args: ~a" xs))
+     (curry-lambda xs body)]
+    [(App f args)
+     (displayln (format "Currying application of: ~a with args: ~a" f args))
+     (curry-application f args)]
     [_ (transform e)]))
 
 (define (transform e)
@@ -45,14 +52,25 @@
     [(Let x e1 e2) (Let x (transform e1) (transform e2))]
     [_ e]))
 
-
 (module+ test
   (require rackunit)
 
-  (check-equal? (unparse (cond->if (parse '(cond [(zero? (- 6 5)) 1]
-                                                 [(<= 6 7)        2]
-                                                 [else            3]))))
-                '(if (zero? (- 6 5)) 1 (if (<= 6 7) 2 3)))
+  ;; Basic zero-parameter lambda
+  (check-equal? (unparse (currify (parse '(λ () 5))))
+                '(λ () 5))
 
-  (check-equal? (unparse (currify (parse '((λ (x y) (+ x y)) 2 3))))
-                '(((λ (x) (λ (y) (+ x y))) 2) 3)))
+  ;; Currying and application in a let expression
+  (check-equal? (unparse (currify (parse '(let ((div (λ (x y) (/ x y)))) (div 4 2)))))
+                '(let ((div (λ (x) (λ (y) (/ x y))))) ((div 4) 2)))
+
+  ;; Currying and application with an if condition
+  (check-equal? (unparse (currify (parse '(if #t 4 ((λ (x y) (/ x y)) 4 2)))))
+                '(if #t 4 (((λ (x) (λ (y) (/ x y))) 4) 2)))
+
+  ;; Complex nested applications with multiple levels of lambdas
+  (check-equal? (unparse (currify (parse '(((λ (x y) (λ (z) (+ z (/ x y)))) 4 2) 5))))
+                '((((λ (x) (λ (y) (λ (z) (+ z (/ x y))))) 4) 2) 5))
+
+  ;; Nested currying with further function applications
+  (check-equal? (unparse (currify (parse '((λ (x y) (/ x y)) 4 ((λ (x y) (+ x y)) 2 3)))))
+                '(((λ (x) (λ (y) (/ x y))) 4) (((λ (x) (λ (y) (+ x y))) 2) 3))))
