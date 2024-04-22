c02f351523265b73680e8d21fec60bfc9200f919
Tanner Helton <tanner@tannerhelton.com>
2024-04-22 12:20:36 -0500

    10.9/20

--- hw4/rewriter.rkt
+++ hw4/rewriter.rkt
@@ -9,32 +9,41 @@
     [(Cond cs else-expr)
      (define (cond->if-helper clauses)
        (match clauses
-         [(list) else-expr] 
+         [(list) (transform else-expr)]
          [(cons (list pred action) rest)
-          (If pred action (cond->if-helper rest))]))
+          (If (transform pred) (transform action) (cond->if-helper rest))]))
      (cond->if-helper cs)]
-    [_ e])) 
-
-
+    [_ (transform e)]))
 
 (define (currify e)
   (match e
-    [(Lam xs body)
+    [(Lam xs body) 
      (define (nest-lambdas variables)
        (match variables
-         [(list) body]
+         [(list) body] 
          [(list x) (Lam (list x) body)]
          [(cons x rest) (Lam (list x) (nest-lambdas rest))]))
      (nest-lambdas xs)]
-    [(App ef es)
+    [(App ef es) 
      (define (nest-applications f args)
        (match args
          [(list) f] 
          [(list arg) (App f arg)]
          [(cons arg1 rest) (nest-applications (App f arg1) rest)]))
-     (nest-applications ef es)]
-    [_ e])) 
+     (nest-applications (transform ef) (map transform es))]
+    [_ (transform e)]))
 
+
+(define (transform e)
+  (match e
+    [(Cond cs else-expr) (cond->if e)]
+    [(If e1 e2 e3) (If (transform e1) (transform e2) (transform e3))]
+    [(App f args) (App (transform f) (map transform args))]
+    [(Lam xs body) (Lam xs (transform body))]
+    [(BinOp op e1 e2) (BinOp op (transform e1) (transform e2))]
+    [(UnOp op e1) (UnOp op (transform e1))]
+    [(Let x e1 e2) (Let x (transform e1) (transform e2))]
+    [_ e]))
 
 
 (module+ test
